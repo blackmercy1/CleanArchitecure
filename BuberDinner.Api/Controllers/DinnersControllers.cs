e3775d87ccef964d60c7b1eaaab47a2a6dcf263c
BLACKmercy1 <leshaar14@gmail.com>
2024-05-06 00:07:54 +0200

    refactor: DinnersControllers.cs attribute refactor

--- BuberDinner.Api/Controllers/DinnersControllers.cs
+++ BuberDinner.Api/Controllers/DinnersControllers.cs
@@ -1,13 +1,11 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
 
-[Authorize]
 [Route("[controller]")]
 public class DinnersControllers : ApiController
 {
-    [HttpGet("dinners"), Route("/dinners")]
+    [HttpGet]
     public IActionResult ListDinners()
     {
         return Ok(Array.Empty<string>());
